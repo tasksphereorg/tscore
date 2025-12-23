3017745786612d98434bfcb9955a25520af407e2
Ajay Wagh <ajay@kali.kali>
2025-12-24 04:06:20 +0530

    Health check file update to test the CICD

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -5,7 +5,7 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 const healthcheck = asyncHandler( async (req, res)=>{
     return res
         .status(200)
-        .json(new ApiResponse(200,"OK", "Health check passed"))
+        .json(new ApiResponse(200,"OK", "Health check passed..."))
 })
 
 
